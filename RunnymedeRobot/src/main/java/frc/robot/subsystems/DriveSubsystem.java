94e4869b355580467f85d0ba02f7647dabc5eb7e
Richard McMullin <richard.e.mcmullin@gmail.com>
2021-12-15 17:43:48 -0500

    robot2 branch

--- RunnymedeRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ RunnymedeRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,22 +1,20 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
 
     // The motors on the left side of the drive.
-    private final TalonSRX leftMotors =
-            new TalonSRX(DriveConstants.LEFT_MOTOR_PORT);
+    private final CANSparkMax leftMotors =
+            new CANSparkMax(DriveConstants.LEFT_MOTOR_PORT, MotorType.kBrushless);
 
     // The motors on the right side of the drive.
-    private final TalonSRX rightMotors =
-            new TalonSRX(DriveConstants.RIGHT_MOTOR_PORT);
+    private final CANSparkMax rightMotors =
+            new CANSparkMax(DriveConstants.RIGHT_MOTOR_PORT, MotorType.kBrushless);
 
     /** Creates a new DriveSubsystem. */
     public DriveSubsystem() {
@@ -26,12 +24,6 @@ public class DriveSubsystem extends SubsystemBase {
         leftMotors.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
         rightMotors.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
 
-        leftMotors.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0,  0);
-        leftMotors.setSelectedSensorPosition(0, 0, 0);
-
-        rightMotors.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0,  0);
-        rightMotors.setSelectedSensorPosition(0, 0, 0);
-
     }
 
     /**
@@ -40,7 +32,7 @@ public class DriveSubsystem extends SubsystemBase {
      * @return the average of the two encoder readings
      */
     public double getAverageEncoderDistance() {
-        return (leftMotors.getSelectedSensorPosition() + rightMotors.getSelectedSensorPosition()) / 2;
+        return 0;
     }
 
     /**
@@ -49,7 +41,7 @@ public class DriveSubsystem extends SubsystemBase {
      * @return the left drive encoder
      */
     public double getLeftEncoder() {
-        return leftMotors.getSelectedSensorPosition();
+        return 0;
     }
 
     /**
@@ -58,24 +50,22 @@ public class DriveSubsystem extends SubsystemBase {
      * @return the right drive encoder
      */
     public double getRightEncoder() {
-        return rightMotors.getSelectedSensorPosition();
+        return 0;
     }
 
     /** Resets the drive encoders to currently read a position of 0. */
     public void resetEncoders() {
-        leftMotors.setSelectedSensorPosition(0);
-        rightMotors.setSelectedSensorPosition(0);
     }
 
     public void setMotorSpeeds(double leftSpeed, double rightSpeed) {
-        leftMotors.set(ControlMode.PercentOutput, leftSpeed);
-        rightMotors.set(ControlMode.PercentOutput, rightSpeed);
+        leftMotors.set(leftSpeed);
+        rightMotors.set(rightSpeed);
     }
 
     @Override
     public void periodic() {
 
-        SmartDashboard.putNumber("Right Motor", rightMotors.getMotorOutputPercent());
-        SmartDashboard.putNumber("Left  Motor", leftMotors.getMotorOutputPercent());
+        //        SmartDashboard.putNumber("Right Motor", rightMotors.get());
+        //        SmartDashboard.putNumber("Left  Motor", leftMotors.get());
     }
 }
