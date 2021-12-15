7ee7d02df2deef6d04884a715b8bd4809ef2bf38
christinakemp <christina.kemp@tdsb.on.ca>
2021-12-15 18:53:33 -0500

    Carousel Package

--- RunnymedeRobot/src/main/java/frc/robot/RobotContainer.java
+++ RunnymedeRobot/src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.OiConstants;
-import frc.robot.commands.drive.DefaultCarouselCommand;
+import frc.robot.commands.carousel.DefaultCarouselCommand;
 import frc.robot.commands.drive.DefaultDriveCommand;
 import frc.robot.subsystems.CarouselSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
--- RunnymedeRobot/src/main/java/frc/robot/commands/carousel/DefaultCarouselCommand.java
+++ RunnymedeRobot/src/main/java/frc/robot/commands/carousel/DefaultCarouselCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.drive;
+package frc.robot.commands.carousel;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
